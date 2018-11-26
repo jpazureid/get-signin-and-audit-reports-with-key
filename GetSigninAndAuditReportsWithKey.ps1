ead3b03092ccaa167962e04abc64102f60a77c37
Jun Takata <36251880+juntakata@users.noreply.github.com>
2018-11-26 14:05:53 +0900

    Refactoring

--- GetSigninAndAuditReportsWithKey.ps1
+++ GetSigninAndAuditReportsWithKey.ps1
@@ -38,101 +38,73 @@ Function Get-AuthResultClientCredentials {
     return $authResult
 }
 
+Function Get-AzureAdLogWithClientCredentials {
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory=$true)]
+        [string]$endpointUrl,
+        
+        [parameter(Mandatory=$true)]
+        [string]$filename
+    )
+
+    $data = @()
+    $authResult = Get-AuthResultClientCredentials -tenantId $tenantId -clientId $clientId -clientSecret $clientSecret -resource $resource
+
+    Do {
+        if ($null -eq $authResult.access_token) {
+            Write-Host "ERROR: No Access Token"
+            Write-Host "Exit"
+            exit
+        }
+
+        #
+        # Compose the access token type and access token for authorization header
+        #
+        $headerParams = @{'Authorization' = "$($authResult.token_type) $($authResult.access_token)"}
+
+        Write-Output "Fetching log data..."
+
+        $myReport = (Invoke-WebRequest -UseBasicParsing -Headers $headerParams -Uri $endpointUrl)
+        $convertedReport = ($myReport.Content | ConvertFrom-Json).value
+        $convertedReportCount = $convertedReport.Count
+
+        for ($j = 0; $j -lt $convertedReportCount; $j++) {
+            $data += $convertedReport[$j]
+        }
+        
+        #
+        # Get url from next link
+        #
+        $endpointUrl = ($myReport.Content | ConvertFrom-Json).'@odata.nextLink'
+
+        $origin = New-Object -Type DateTime -ArgumentList 1970, 1, 1, 0, 0, 0, 0
+        $expiresOn = $origin.AddSeconds($authResult.expires_on).AddSeconds(-300) # minus 5 min
+        $getdate = (Get-Date).ToUniversalTime()
+
+        #
+        # Acquire token again if it expires soon
+        #
+        if ($expiresOn -lt $getdate) {
+            $authResult = Get-AuthResultClientCredentials -tenantId $tenantId -clientId $clientId -clientSecret $clientSecret -resource $resource
+        }
+    } while ($null -ne $endpointUrl -And "" -ne $endpointUrl)
+
+    $data | Sort-Object -Property createdDateTime  | Export-Csv $filename -encoding "utf8" -NoTypeInformation
+    Write-Output "log data is exported to $filename"
+}
+
+$signInsUrl = "$resource/beta/auditLogs/signIns"
+Get-AzureAdLogWithClientCredentials -endpointUrl $signInsUrl -filename $outputSigninLogFile
+
 #
-# Get sign in log
+# Sample queries are as below
 #
-$data = @()
-$url = "$resource/beta/auditLogs/signIns"
+#$signInsUrl = "$resource/beta/auditLogs/signIns?`$filter=deviceDetail/operatingSystem eq 'Windows 10'"
+#$signInsUrl = "$resource/beta/auditLogs/signIns?`$filter=startswith(deviceDetail/browser,'IE')"
+#$signInsUrl = "$resource/beta/auditLogs/signIns?`$filter=location/city eq 'Sattle'"
+#$signInsUrl = "$resource/beta/auditLogs/signIns?`$filter=location/state eq 'Bayern'"
+#$signInsUrl = "$resource/beta/auditLogs/signIns?`$filter=location/countryOrRegion eq 'JP'"
 
-$authResult = Get-AuthResultClientCredentials -tenantId $tenantId -clientId $clientId -clientSecret $clientSecret -resource $resource
-
-Do {
-    if ($null -eq $authResult.access_token) {
-        Write-Host "ERROR: No Access Token"
-        Write-Host "Exit"
-        exit
-    }
-
-    #
-    # Compose the access token type and access token for authorization header
-    #
-    $headerParams = @{'Authorization' = "$($authResult.token_type) $($authResult.access_token)"}
-
-    Write-Output "Fetching signin log data..."
-
-    $myReport = (Invoke-WebRequest -UseBasicParsing -Headers $headerParams -Uri $url)
-    $convertedReport = ($myReport.Content | ConvertFrom-Json).value
-    $convertedReportCount = $convertedReport.Count
-
-    for ($j = 0; $j -lt $convertedReportCount; $j++) {
-        $data += $convertedReport[$j]
-    }
-    
-    #
-    # Get url from next link
-    #
-    $url = ($myReport.Content | ConvertFrom-Json).'@odata.nextLink'
-
-    $origin = New-Object -Type DateTime -ArgumentList 1970, 1, 1, 0, 0, 0, 0
-    $expiresOn = $origin.AddSeconds($authResult.expires_on).AddSeconds(-300) # minus 5 min
-    $getdate = (Get-Date).ToUniversalTime()
-
-    #
-    # Acquire token again if it expires soon
-    #
-    if ($expiresOn -lt $getdate) {
-        $authResult = Get-AuthResultClientCredentials($tenantId, $clientId, $clientSecret, $resource)
-    }
-} while ($null -ne $url)
-
-$data | Sort-Object -Property createdDateTime  | Export-Csv $outputSigninLogFile -encoding "utf8" -NoTypeInformation
-Write-Output "Sign-in log data is exported to $outputSigninLogFile"
-
-#
-# Audit log
-# Compose the access token type and access token for authorization header
-#
-$data = @()
-$url = "$resource/beta/auditLogs/directoryAudits"
-
-Do {
-    if ($null -eq $authResult.access_token) {
-        Write-Host "ERROR: No Access Token"
-        Write-Host "Exit"
-        exit
-    }
-    
-    #
-    # Compose the access token type and access token for authorization header
-    #
-    $headerParams = @{'Authorization' = "$($authResult.token_type) $($authResult.access_token)"}
-
-    Write-Output "Fetching audit log data..."
-
-    $myReport = (Invoke-WebRequest -UseBasicParsing -Headers $headerParams -Uri $url)
-    $convertedReport = ($myReport.Content | ConvertFrom-Json).value
-    $convertedReportCount = $convertedReport.Count
-
-    for ($j = 0; $j -lt $convertedReportCount; $j++) {
-        $data += $convertedReport[$j]
-    }
-    
-    #
-    # Get url from next link
-    #
-    $url = ($myReport.Content | ConvertFrom-Json).'@odata.nextLink'
-
-    $origin = New-Object -Type DateTime -ArgumentList 1970, 1, 1, 0, 0, 0, 0
-    $expiresOn = $origin.AddSeconds($authResult.expires_on).AddSeconds(-300) # minus 5 min
-    $getdate = (Get-Date).ToUniversalTime()
-    
-    #
-    # Acquire token again if it expires soon
-    #
-    if ($expiresOn -lt $getdate) {
-        $authResult = Get-AuthResultClientCredentials($tenantId, $clientId, $clientSecret, $resource)
-    }
-}while ($null -ne $url)
-
-$data | Sort-Object -Property createdDateTime  | Export-Csv $outputAuditLogFile -encoding "utf8" -NoTypeInformation
-Write-Output "Sign-in log data is exported to $outputAuditLogFile"
+$directoryAuditsUrl = "$resource/beta/auditLogs/directoryAudits"
+Get-AzureAdLogWithClientCredentials -endpointUrl $directoryAuditsUrl -filename $outputAuditLogFile
